f91a131bb969d4b2352054f3f955143e2ecbbcb7
stevetsala22 <stevetsala22@gmail.com>
2026-03-11 13:16:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE='ptboard-v1';
+const ASSETS=[
+  '/ptboard/PTBoard_v8.html',
+  '/ptboard/manifest.json'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  // Network first, fallback to cache
+  e.respondWith(
+    fetch(e.request)
+      .then(res=>{
+        const clone=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+        return res;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
